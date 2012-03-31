87e858ac1650365d8130d1fa5f650cb2bfc4f189
Dmitry Promsky <dmitry.promsky@gmail.com>
2012-04-01 03:20:05 +0400

    Only launch last.fm client if it isn't already running (prevents last.fm client's window from popping up on Cog restarts)

--- AudioScrobbler/AudioScrobbler.m
+++ AudioScrobbler/AudioScrobbler.m
@@ -70,9 +70,22 @@ escapeForLastFM(NSString *string)
 	if((self = [super init])) {
 
 		_pluginID = @"cog";
-
-		if([[NSUserDefaults standardUserDefaults] boolForKey:@"automaticallyLaunchLastFM"])
-			[[NSWorkspace sharedWorkspace] launchApplication:@"Last.fm.app"];
+                
+		if([[NSUserDefaults standardUserDefaults] boolForKey:@"automaticallyLaunchLastFM"]) {
+            
+            NSArray *launchedApps = [[NSWorkspace sharedWorkspace] runningApplications];
+            BOOL running = NO;
+            for(NSRunningApplication *app in launchedApps) {
+                if([[app bundleIdentifier] isEqualToString:@"fm.last.Last.fm"]) {
+                    running = YES;
+                    break;
+                }
+            }
+            
+            if(!running) {
+                [[NSWorkspace sharedWorkspace] launchApplication:@"Last.fm.app"];
+            }
+        }
 		
 		_keepProcessingAudioScrobblerCommands = YES;
 
