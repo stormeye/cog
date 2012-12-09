754d60c5951ea0fc1a443aca8ecd4591d8c190f9
Dmitry Promsky <dmitry.promsky@gmail.com>
2012-12-10 00:58:41 +0400

    Info window now shows info for currently playing track in mini mode

--- InfoInspector/InfoWindowController.m
+++ InfoInspector/InfoWindowController.m
@@ -8,10 +8,12 @@
 
 #import "InfoWindowController.h"
 #import "MissingAlbumArtTransformer.h"
+#import "Logging.h"
+#import "AppController.h"
 
 @implementation InfoWindowController
 
-@synthesize playlistSelectionController;
+@synthesize valueToDisplay;
 
 + (void)initialize
 {
@@ -24,13 +26,41 @@
 {
 	return [super initWithWindowNibName:@"InfoInspector"];
 }
-		
+
+- (void)awakeFromNib
+{
+    [playlistSelectionController addObserver:self forKeyPath:@"selection" options:NSKeyValueObservingOptionNew context:nil];
+    [currentEntryController addObserver:self forKeyPath:@"content" options:NSKeyValueObservingOptionNew context:nil];
+    [appController addObserver:self forKeyPath:@"miniMode" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    BOOL miniMode = [appController miniMode];
+    BOOL currentEntryChanged = (object == currentEntryController && [keyPath isEqualTo:@"content"]);
+    BOOL selectionChanged = (object == playlistSelectionController && [keyPath isEqualTo:@"selection"]);
+    BOOL miniModeSwitched = (object == appController && [keyPath isEqual:@"miniMode"]);
+
+    if (miniMode && (currentEntryChanged || miniModeSwitched))
+    {
+        [self setValueToDisplay:[currentEntryController content]];
+    }
+    else if (!miniMode && (selectionChanged || miniModeSwitched))
+    {
+        [self setValueToDisplay:[playlistSelectionController selection]];
+    }
+}
+
 - (IBAction)toggleWindow:(id)sender
 {
 	if ([[self window] isVisible])
+    {
 		[[self window] orderOut:self];
+    }
 	else
+    {
 		[self showWindow:self];
+    }
 }
 
 @end
